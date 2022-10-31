a22b6a51f4e051a8198eb1d37e244b57c68a276c
miserior <51886505+miserior@users.noreply.github.com>
2022-10-31 16:12:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+from streamlit_pandas_profiling import st_profile_report
 
-st.title("motor vehicle collissions in new york")
-st.markdown("this application is a streamlit dashboard")
+
+
+with st.sidebar:
+    st.title("Menu principal")
+    try:
+        add_radio = st.radio("Seleccione el formato",("CSV", "EXCEL","JSON"))
+        
+        if add_radio == "CSV":
+            file = st.file_uploader("Seleccione el archivo")
+            df = pd.read_csv(file)
+        if add_radio == "EXCEL":
+            file = st.file_uploader("Seleccione el archivo")
+            df = pd.read_excel(file)
+        if add_radio == "JSON":
+            title = st.text_input("Ingresa la URL aquí 👇")
+            df = pd.read_json(title)
+    except:
+        pass
+
+st.title("Perfilamiento de datos ")
+try:
+    pr = df.profile_report()
+    st_profile_report(pr)
+except:
+    pass
