8ec9383a7891ced32dcb8ac735ccae6fc4de0fa8
miserior <51886505+miserior@users.noreply.github.com>
2022-10-31 20:41:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,26 +10,27 @@ from streamlit_pandas_profiling import st_profile_report
 with st.sidebar:
     st.title("Menu principal")
     try:
+        
         add_radio = st.radio("Seleccione el formato",("CSV", "EXCEL","JSON"))
         
-        if add_radio == "CSV":
-            file = st.file_uploader("Seleccione el archivo")
-            df = pd.read_csv(file)
-        if add_radio == "EXCEL":
-            file = st.file_uploader("Seleccione el archivo")
-            df = pd.read_excel(file)
-        if add_radio == "JSON":
-            title = st.text_input("Ingresa la URL aquí 👇")
-            df = pd.read_json(title)
+        @st.cache(persist=True)
+        def perfilamiento(add_radio):
+            if add_radio == "CSV":
+                file = st.file_uploader("Seleccione el archivo")
+                df = pd.read_csv(file)
+            if add_radio == "EXCEL":
+                file = st.file_uploader("Seleccione el archivo")
+                df = pd.read_excel(file)
+            if add_radio == "JSON":
+                title = st.text_input("Ingresa la URL aquí 👇")
+                df = pd.read_json(title)
     except:
         pass
 
 st.title("Perfilamiento de datos ")
 
-@st.cache(persist=True)
-def perfilamiento(df):
-    try:
-        pr = df.profile_report()
-        st_profile_report(pr)
-    except:
-        pass
+try:
+    pr = df.profile_report()
+    st_profile_report(pr)
+except:
+    pass
