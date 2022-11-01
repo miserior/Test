2484691f351532144e5b1fdfc6cc5839f1719b9f
miserior <51886505+miserior@users.noreply.github.com>
2022-10-31 21:52:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,19 +10,19 @@ if add_radio == "CSV" or add_radio == "EXCEL":
     file = st.file_uploader("Seleccione el archivo")
 if add_radio == "JSON":
     title = st.text_input("Ingresa la URL aquí 👇")                         
-@st.cache(persist=True)
-def load_data():
-    if add_radio == "CSV":
-        df = pd.read_csv(file)
-    if add_radio == "EXCEL":
-        df = pd.read_excel(file)
-    if add_radio == "JSON":
-        df = pd.read_json(title)
-    return df
-
-df = load_data()
-st.title("Perfilamiento de datos ")
 try:
+    @st.cache(persist=True)
+    def load_data():
+        if add_radio == "CSV":
+            df = pd.read_csv(file)
+        if add_radio == "EXCEL":
+            df = pd.read_excel(file)
+        if add_radio == "JSON":
+            df = pd.read_json(title)
+        return df
+
+    df = load_data()
+    st.title("Perfilamiento de datos ")
     pr = df.profile_report()
     st_profile_report(pr)
 except:
