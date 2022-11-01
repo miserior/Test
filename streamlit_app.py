434cce35447e6305f4dae6f2b2f3d27c2a5432aa
miserior <51886505+miserior@users.noreply.github.com>
2022-10-31 21:13:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,24 @@ import numpy as np
 import pydeck as pdk
 from streamlit_pandas_profiling import st_profile_report
 
-
-add_radio = st.radio("Seleccione el formato",("CSV", "EXCEL","JSON"))
-if add_radio == "CSV" or add_radio == "EXCEL":
-    file = st.file_uploader("Seleccione el archivo")
-if add_radio == "JSON":
-    title = st.text_input("Ingresa la URL aquí 👇")
-    
-@st.cache
-def load_data():
-    if add_radio == "CSV":
-        df = pd.read_csv(file)
-    if add_radio == "EXCEL":
-        df = pd.read_excel(file)
+with st.sidebar:
+    st.title("Menu principal")
+    add_radio = st.radio("Seleccione el formato",("CSV", "EXCEL","JSON"))
+    if add_radio == "CSV" or add_radio == "EXCEL":
+        file = st.file_uploader("Seleccione el archivo")
     if add_radio == "JSON":
-        df = pd.read_json(title)
-    return df
-df = load_data()
+        title = st.text_input("Ingresa la URL aquí 👇")
+    
+    @st.cache
+    def load_data():
+        if add_radio == "CSV":
+            df = pd.read_csv(file)
+        if add_radio == "EXCEL":
+            df = pd.read_excel(file)
+        if add_radio == "JSON":
+            df = pd.read_json(title)
+        return df
+    df = load_data()
 
 st.title("Perfilamiento de datos ")
 try:
